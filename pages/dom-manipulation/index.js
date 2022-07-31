3e9dbb850ffe5df75d7a4cc42b2c28381086df08
nyan-ko <dumbstupidcatboy@gmail.com>
2022-07-31 13:01:26 -0400

    Finish playing with DOM/events

--- pages/dom-manipulation/index.js
+++ pages/dom-manipulation/index.js
@@ -21,10 +21,40 @@ h1.textContent = "I'm in a div!";
 const p2 = document.createElement("p");
 p2.textContent = "ME TOO!";
 
+const butt = document.createElement("button"); // method 1 for events
+butt.setAttribute("onclick", "alert('Hello world')");
+butt.textContent = "click me";
+
+const butt2 = document.createElement("button");
+butt2.textContent = "click me 2";
+butt2.onclick = () => {
+    alert("hello world lol");
+}
+
+const butt3 = document.createElement("button");
+butt3.textContent = "click me 3";
+butt3.addEventListener('click', () => {
+    alert("hello world 3");
+});
+butt3.addEventListener('click', function(e) {
+    console.log(e.target);
+    e.target.style.backgroundColor = "pink";
+});
+
 div2.appendChild(h1);
 div2.appendChild(p2);
+div2.appendChild(butt);
+div2.appendChild(butt2);
+div2.appendChild(butt3);
 
 body.appendChild(div);
 div.appendChild(p);
 div.appendChild(h3);
 div.appendChild(div2);
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert('meow');
+    })
+});
